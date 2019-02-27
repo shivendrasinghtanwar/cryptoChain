e7239489fc26152d063e9542a6cc814813b81954
Shivendra <shivendra@ipropal.com>
2019-02-27 11:33:32 +0530

    Add channels in blockchain

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
+const PubSub = require('./pubsub');
+const request = require('request');
+
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain});
+const DEFAULT_PORT = 3500;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+// setTimeout(() => pubsub.broadcastChain(),1000);
 
 app.use(bodyParser.json());
 
@@ -16,10 +24,32 @@ app.post('/api/mine',(req,res)=>{
 
     blockchain.addBlock({ data });
 
+    pubsub.broadcastChain();
     res.redirect('/api/blocks');
 });
 
-const port = 3500;
+const syncChain = ( ) => {
+    request({ url : `${ROOT_NODE_ADDRESS}/api/blocks` },(error,response,body) => {
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+
+            console.log("Replace chain on sync with",rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true'){
+    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+const port = PEER_PORT || DEFAULT_PORT;
 app.listen(port,()=>{
     console.log(`Listening at localhost:${port}`)
+
+    if(port !== DEFAULT_PORT){
+        syncChain();
+    }
+    
 });
