a0ebcd8b923cff3e1551c36eddd0627772843136
Shivendra <shivendra@ipropal.com>
2019-03-11 18:42:16 +0530

    Corrected gitignore and started react

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<
-    div>cryotoChain in react</div>, 
+render(
+    <div>cryotoChain in react</div>, 
     document.getElementById('root')
     );
