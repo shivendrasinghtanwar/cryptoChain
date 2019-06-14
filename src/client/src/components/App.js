14edd105a772290525eae1be57a488d34d53d354
Shivendra <shivendra@ipropal.com>
2019-06-14 16:33:36 +0530

    Add displayed wallet info on frontend

---
+++ src/client/src/components/App.js
@@ -0,0 +1,39 @@
+import React,{ Component } from 'react';
+
+class App extends Component{
+
+    state = {
+        walletInfo:{
+            address:'',
+            balance: 0
+        }
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:3500/api/wallet/info')
+        .then(Response => Response.json())
+        .then(json => this.setState({
+            walletInfo: json
+        }));
+        
+        
+    }
+
+    render(){
+        const { address,balance } = this.state.walletInfo;
+
+        return(
+            <div>
+                <h3>
+                    Welcome to Blockchain!!!
+                </h3>
+                <div>
+                    Address: {address}
+                </div>
+                <div> Balance: {balance} </div>
+            </div>
+        )
+    }
+}
+
+export default App;
